7c480b50663b8abd1e16cbf804db8feb96eaca1c
jp-ryosuke <64393743+jp-ryosuke@users.noreply.github.com>
2023-02-08 23:15:53 +0900

    the first commit
    "駅すぱぁと"のURL生成テンプレ用

---
+++ index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+var URL = 'http://api.ekispert.jp/v1/json/search/course/light?';
+
+request.get({
+    uri: URL,
+    headers: {'Content-type': 'application/json'},
+    qs: {
+        // GETのURLの後に付く ?hoge=hugaの部分
+        key : '',   //API key
+        from : '',              //始点の駅
+        to : '',              //目的の駅
+    },
+    json: true
+}, function(err, req, data){
+    //console.log(err);
+    //console.log(req);
+    //console.log(data);
+    
+    let url = data.ResultSet.ResourceURI;
+    console.log(url);
+});
